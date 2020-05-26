dec6c86c710142bb6649e4ddf13400e7e1e5f204
ch-mk <mark.cherp@gmail.com>
2020-05-25 18:48:45 -0700

    added initial setup

---
+++ Labs/Setup/install_preqs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt install -y net-tools
+
+sudo apt install -y python
+
+sudo apt install -y curl
+
+sudo apt install -y make
+
+sudo apt install -y build-essential
+
+sudo snap install --classic code
+# install C\C++ extension
+# install CodeLLDB extension
+
+sudo setfacl -m u:${USER}:rw /dev/kvm
+
+sudo apt install -y iperf3
+
+sudo apt install -y gnuplot-x11
+
+sudo snap install --classic kubectl 
+
+sudo snap install --classic  go
+
+curl https://sh.rustup.rs -sSf | sh
